273d4df62e75bcdca0e0df4ae59a190b36689a65
311298 <kashyapanurag109@gmail.com>
2022-09-23 15:44:11 +0530

    post and get

--- app.component.ts
+++ app.component.ts
@@ -1,28 +1,71 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { map } from "rxjs/operators";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent implements OnInit {
   loadedPosts = [];
+  array2BPushed: any[] = [];
 
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.fetchPosts();
+  }
 
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
     console.log(postData);
+
+    this.http
+      .post(
+        "https://https-3581e-default-rtdb.asia-southeast1.firebasedatabase.app/posts.json",
+        postData
+      )
+      .subscribe((responseData) => {
+        console.log("response data we sent -> ", responseData);
+      });
   }
+  //post takes 2 arguments url and data
+  //2 post request are send first one is option ( check whether allowed or not) and another request is we send body
 
   onFetchPosts() {
     // Send Http request
+    this.fetchPosts();
   }
 
   onClearPosts() {
     // Send Http request
   }
+
+  private fetchPosts() {
+    this.http
+      .get(
+        "https://https-3581e-default-rtdb.asia-southeast1.firebasedatabase.app/posts.json"
+      )
+      .pipe(
+        map((data) => {
+          console.log("data before transform ->", data);
+          console.log("key from data", Object.keys(data));
+          const postArray = [];
+          for (const key in data) {
+            if (data.hasOwnProperty(key)) {
+              console.log("key of the object(data) -> ", key);
+              postArray.push({ ...data[key], id: key });
+            }
+          }
+          return postArray;
+        })
+      )
+      .subscribe((post) => {
+        console.log("posts we fetched -> ", post);
+        console.log("loaded posts -> ", this.loadedPosts);
+      });
+  }
 }
+// fetch post methods need to be called inside the fetch method and also inside the ngOnint life cycle hook
+// it also need to subscibe in order to work
--- app.module.ts
+++ app.module.ts
@@ -1,14 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, FormsModule, HttpClientModule],
+  // httpclientmodule gives us the http tools
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
