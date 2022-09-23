de83225c59e97761b3acac18ee3e0f33c666bf00
311298 <kashyapanurag109@gmail.com>
2022-09-23 17:01:33 +0530

    using the service for create , fetch and delete posts

--- app.component.ts
+++ app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { map } from "rxjs/operators";
 import { post } from "./post.model";
+import { PostService } from "./post.service";
 
 @Component({
   selector: "app-root",
@@ -12,66 +13,39 @@ export class AppComponent implements OnInit {
   loadedPosts = [];
   isFetching: boolean = false;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private ps: PostService) {}
 
   ngOnInit() {
-    this.fetchPosts();
+    this.onFetchPosts();
   }
 
   // onCreatePost(postData: { title: string; content: string }) {
   onCreatePost(postData: post) {
     // Send Http request
     console.log(postData);
-
-    this.http
-      .post<{ name: string }>(
-        // here were are passing string not form object, it is firebase special key
-        "https://https-3581e-default-rtdb.asia-southeast1.firebasedatabase.app/posts.json",
-        postData
-      )
+    this.ps
+      .createPosts(postData.title, postData.content)
       .subscribe((responseData) => {
-        console.log("response data we sent -> ", responseData);
+        console.log(responseData);
       });
   }
   //post takes 2 arguments url and data
   //2 post request are send first one is option ( check whether allowed or not) and another request is we send body
 
   onFetchPosts() {
+    this.isFetching = true;
     // Send Http request
-    this.fetchPosts();
+    this.ps.fetchPosts().subscribe((responseData) => {
+      this.loadedPosts = responseData;
+      this.isFetching = false;
+    });
   }
 
   onClearPosts() {
     // Send Http request
-  }
-
-  private fetchPosts() {
-    this.isFetching = true;
-    this.http
-      .get<{ [key: string]: post }>(
-        "https://https-3581e-default-rtdb.asia-southeast1.firebasedatabase.app/posts.json"
-      )
-      .pipe(
-        // map((responseData: { [key: string]: post }) => {
-        //instead of defining inside the reponseData we can define it inside the https methods
-        map((responseData) => {
-          console.log("data before transform ->", responseData);
-          console.log("key from data", Object.keys(responseData));
-          const postArray: post[] = [];
-          for (const key in responseData) {
-            if (responseData.hasOwnProperty(key)) {
-              console.log("key of the object(data) -> ", key);
-              postArray.push({ ...responseData[key], id: key });
-            }
-          }
-          return postArray;
-        })
-      )
-      .subscribe((post) => {
-        this.isFetching = false;
-        console.log("posts we fetched -> ", post);
-        this.loadedPosts = post;
-      });
+    this.ps.deletePosts().subscribe(() => {
+      this.loadedPosts = [];
+    });
   }
 }
 // fetch post methods need to be called inside the fetch method and also inside the ngOnint life cycle hook
--- post.service.ts
+++ post.service.ts
@@ -1,6 +1,55 @@
+import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { post } from "./post.model";
+import { map } from "rxjs/operators";
 
 @Injectable({ providedIn: "root" })
-export class Postservice {
-  createPosts() {}
+export class PostService {
+  constructor(private http: HttpClient) {}
+
+  createPosts(title: string, content: string) {
+    const postData = { title: title, content: content };
+    return this.http.post<{ name: string }>(
+      // here were are passing string not form object, it is firebase special key
+      "https://https-3581e-default-rtdb.asia-southeast1.firebasedatabase.app/posts.json",
+      postData
+    );
+    //   .subscribe((responseData) => {
+    //     console.log("response data we sent -> ", responseData);
+    //   });
+  }
+
+  deletePosts() {
+    return this.http.delete(
+      "https://https-3581e-default-rtdb.asia-southeast1.firebasedatabase.app/posts.json"
+    );
+  }
+
+  fetchPosts() {
+    return this.http
+      .get<{ [key: string]: post }>(
+        "https://https-3581e-default-rtdb.asia-southeast1.firebasedatabase.app/posts.json"
+      )
+      .pipe(
+        // map((responseData: { [key: string]: post }) => {
+        //instead of defining inside the reponseData we can define it inside the https methods
+        map((responseData) => {
+          console.log("data before transform ->", responseData);
+          console.log("key from data", Object.keys(responseData));
+          const postArray: post[] = [];
+          for (const key in responseData) {
+            if (responseData.hasOwnProperty(key)) {
+              console.log("key of the object(data) -> ", key);
+              postArray.push({ ...responseData[key], id: key });
+            }
+          }
+          return postArray;
+        })
+      );
+    //   .subscribe((post) => {
+    //     // this.isFetching = false;
+    //     console.log("posts we fetched -> ", post);
+    //     // this.loadedPosts = post;
+    //   });
+  }
 }
